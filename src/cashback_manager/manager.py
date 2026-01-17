d60d43ee71ac8e9968c137324c2bb8eb48defc0b
Egor Leonov <egorgarderobov@gmail.com>
2026-01-17 14:58:16 +0300

    Update manager.py

--- src/cashback_manager/manager.py
+++ src/cashback_manager/manager.py
@@ -6,6 +6,8 @@ class CashbackManager:
         self.storage = storage
 
     def add_cashback(self, bank, cashback: Cashback):
+        if not isinstance(cashback, Cashback):
+            raise TypeError("cashback must be an instance of Cashback")
         if bank not in self.storage.data:
             self.storage.data[bank] = {}
         self.storage.data[bank][cashback.name] = cashback.percent
@@ -20,27 +22,24 @@ class CashbackManager:
         return sorted(results, key=lambda x: x[2], reverse=True)
 
     def delete_cashback(self, bank, cashback_name):
-        if bank in self.storage.data:
-            if cashback_name in self.storage.data[bank]:
-                del self.storage.data[bank][cashback_name]
-                if not self.storage.data[bank]:
-                    del self.storage.data[bank]
-                self.storage.save()
-                if not self.storage.data:
-                    self.storage.delete_file()
-            else:
-                print(f"Cashback '{cashback_name}' not found in '{bank}'")
-        else:
-            print(f"Bank '{bank}' does not exist")
+        if bank not in self.storage.data:
+            raise ValueError(f"Bank '{bank}' does not exist")
+        if cashback_name not in self.storage.data[bank]:
+            raise ValueError(f"Cashback '{cashback_name}' not found in '{bank}'")
+        del self.storage.data[bank][cashback_name]
+        if not self.storage.data[bank]:
+            del self.storage.data[bank]
+        self.storage.save()
+        if not self.storage.data:
+            self.storage.delete_file()
 
     def delete_bank(self, bank):
-        if bank in self.storage.data:
-            del self.storage.data[bank]
-            self.storage.save()
-            if not self.storage.data:
-                self.storage.delete_file()
-        else:
-            print("Bank does not exist.")
+        if bank not in self.storage.data:
+            raise ValueError(f"Bank '{bank}' does not exist")
+        del self.storage.data[bank]
+        self.storage.save()
+        if not self.storage.data:
+            self.storage.delete_file()
 
     def clear_all(self):
         self.storage.data.clear()
